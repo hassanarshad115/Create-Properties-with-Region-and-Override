e63f3ae19b6dd9d744ff472f74109bf39585ff0d
HASSAN ARSHAD <hassanarshad115@gmail.com>
2017-11-21 09:12:56 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        #region  
+
+        static void Main(string[] args)
+        {
+            animal obj = new animal();
+            obj.speak();
+
+            obj = new dog();
+            obj.speak();
+
+
+            dog d = new dog();
+
+            d.speak();
+            Console.ReadKey();
+        }
+#endregion
+    }
+}
---
+++ ConsoleApp1/animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class animal
+    {
+        public virtual void speak()
+        {
+            Console.WriteLine("orignal class");
+        }
+
+    }
+
+    public class dog:animal
+    {
+       public  override void speak()
+        {
+            Console.WriteLine("orignal class sy override hony wali");
+        }
+
+    }
+
+
+}
